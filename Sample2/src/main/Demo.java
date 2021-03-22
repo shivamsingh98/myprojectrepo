93e4b8400f63cd9aaaf4a08846fd0d0430f0d1ac
shivamsingh98 <37807844+shivamsingh98@users.noreply.github.com>
2021-03-22 06:01:55 -0700

    trying git pull

--- Sample2/src/main/Demo.java
+++ Sample2/src/main/Demo.java
@@ -4,7 +4,7 @@ public class Demo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World");
+		System.out.println("Hello World demonstrating git pull");
 		add(10,20);
 	}
 	public static void add(int num1,int num2){
