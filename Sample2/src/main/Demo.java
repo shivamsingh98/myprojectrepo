54738c56d996a4a6b325553b7e6578e9f4c1bd41
shivam <0910shivamsingh@gmail.com>
2021-03-22 22:24:50 -0700

    added master fun2

--- Sample2/src/main/Demo.java
+++ Sample2/src/main/Demo.java
@@ -19,5 +19,7 @@ public class Demo {
 	public static void cube(int num1){
 		System.out.println(num1*num1*num1);
 	}
-	
+	public static void masterfun1(int num1){
+		System.out.println(num1*num1*num1);
+	}
 }
