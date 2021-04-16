39b75e4ed6e229a2f1d0106308d779846bef5dda
shivamsingh98 <37807844+shivamsingh98@users.noreply.github.com>
2021-04-16 12:44:13 -0700

    Update Demo.java

--- Sample2/src/main/Demo.java
+++ Sample2/src/main/Demo.java
@@ -67,7 +67,7 @@ public class Demo {
 	public static void br1fun4(int num1){
 		System.out.println(num1*num1*num1);
 	}
-	public static void masterfun7(int num1){
+	public static void masterfun6(int num1){
 		System.out.println(num1*num1*num1);
 	}
 
